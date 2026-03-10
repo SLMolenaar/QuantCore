50a99f68e17fe408448f50670a6ba2b94aae3245
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2026-03-10 17:46:30 +0100

    Bump version to 0.1.1 and add virtualenv to publish workflow

--- python/setup.py
+++ python/setup.py
@@ -63,7 +63,7 @@ long_description = readme.read_text(encoding="utf-8") if readme.exists() else ""
 
 setup(
     name="quantcore",
-    version="0.1.0",
+    version="0.1.1",
     author="Stefaan Molenaar",
     author_email="StefaanLMolenaar@gmail.com",
     description="High-performance C++20 backtesting engine with Python interface",
