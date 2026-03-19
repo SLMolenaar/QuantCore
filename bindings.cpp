fe927bf4da52c0152407600eb4aa4b2d6c89f826
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2026-03-19 12:43:46 +0100

    Bump version from 0.1.9 to 0.2.0

--- bindings.cpp
+++ bindings.cpp
@@ -511,7 +511,7 @@ PYBIND11_MODULE(_core, m) {
     // ============================================================================
 
     m.def("hello",   []() { return "Hello from QuantCore C++!"; });
-    m.def("version", []() { return "0.1.9"; });
+    m.def("version", []() { return "0.2.0"; });
 
-    m.attr("__version__") = "0.1.9";
+    m.attr("__version__") = "0.2.0";
 }
