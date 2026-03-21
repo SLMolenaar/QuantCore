5e17a229f998853e64f2eb22eeda94d59cdb107c
Stefaan Molenaar <StefaanLMolenaar@gmail.com>
2026-03-21 01:23:50 +0100

    v0.2.2

--- bindings.cpp
+++ bindings.cpp
@@ -513,7 +513,7 @@ PYBIND11_MODULE(_core, m) {
     // ============================================================================
 
     m.def("hello",   []() { return "Hello from QuantCore C++!"; });
-    m.def("version", []() { return "0.2.1"; });
+    m.def("version", []() { return "0.2.2"; });
 
-    m.attr("__version__") = "0.2.1";
+    m.attr("__version__") = "0.2.2";
 }
